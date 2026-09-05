239fd5cbb3accc3fa2dd78f0b1571ca20326be72
shalo10 <Logudigital06@gmail.com>
2026-09-05 11:45:25 +0530

    changed oxygen, hull to 100 and galax size to 10

--- constants.py
+++ constants.py
@@ -10,10 +10,10 @@ SHIP_NAME = "UniStar"
 CREW_DESCRIPTION = "Computer Science rookies debugging the galaxy one line at a time."
 
 # --- Starting resources ---
-STARTING_OXYGEN = 10
-STARTING_HULL = 10
+STARTING_OXYGEN = 100
+STARTING_HULL = 100
 
 # --- Galaxy ---
-GALAXY_SIZE = 8
+GALAXY_SIZE = 10
 USE_CUSTOM_PLANETS = False
 PLANETS = []
